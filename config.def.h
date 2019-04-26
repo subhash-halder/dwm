9dc8ec308f46b6ce20175588f9f8ee2497cf3cf1
Subhash Halder <halder.subhash@gmail.com>
2019-04-26 17:06:11 +0530

    Added opacity patch

--- config.def.h
+++ config.def.h
@@ -12,10 +12,17 @@ static const char col_gray2[]       = "#282a36";
 static const char col_gray3[]       = "#f8f8f2";
 static const char col_gray4[]       = "#282a36";
 static const char col_cyan[]        = "#6272a4";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
